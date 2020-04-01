ff0a1f17c10c4b1236ecda21841267e50bf03195
Kingmercy-Rayne <kingrayne66@gmail.com>
2020-04-01 22:54:30 +0800

    Init Project

---
+++ src/components/Posts.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>Reddit</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Posts',
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    this.load();
+  },
+  methods: {
+    load() {
+      const posts = [];
+      const url = 'https://www.reddit.com/r/programmerhumor/new.json?Limit=100';
+      fetch(url)
+        .then((response) => response.json())
+        .then((result) => {
+          posts.push(result.data.children);
+          console.log(posts);
+        });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
